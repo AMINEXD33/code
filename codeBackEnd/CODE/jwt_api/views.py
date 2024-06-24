fc6d9e31d105c336fa2caa243e2773fdc7f56d50
AMINEXD33 <amine.gamer222@gmail.com>
2024-06-24 23:02:07 +0100

    stable beta server

--- codeBackEnd/CODE/jwt_api/views.py
+++ codeBackEnd/CODE/jwt_api/views.py
@@ -3,8 +3,10 @@ from rest_framework import permissions, viewsets
 from .serializers import GroupSerializer, UserSerializer
 from django.apps import apps
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
 def get_keys():
     reference = apps.get_app_config("jwt_api").my_object
-    
+    token = reference.make_token({"amine":"meftah"})
+
+    return JsonResponse({"token":token})
